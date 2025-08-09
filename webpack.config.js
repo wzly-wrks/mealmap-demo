8106d6c68d27e582449c60334a44b3226cd9ac04
Weezly Works <weezly@weezly.works>
2025-08-09 05:29:49 -0700

    feat: integrate react into build

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = (env, argv) => {
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env']
+              presets: ['@babel/preset-env', '@babel/preset-react']
             }
           }
         },
